a27b23d61a2a10a7cfe3f7521be3b00a2f594c53
James Pozdena <jpoz@jpoz.net>
2010-03-30 22:34:40 -0700

    boom

--- spec/one_spec.rb
+++ spec/one_spec.rb
@@ -4,19 +4,19 @@ describe String do
 
   it "should run" do
     sleep 1
-    true.should be_false
+    true.should be_true
   end
   
   it "should run" do
     sleep 1
-    true.should be_false
+    true.should be_true
   end
   it "should run" do
     sleep 1
-    true.should be_false
+    true.should be_true
   end
   it "should run" do
     sleep 1
-    true.should be_false
+    true.should be_true
   end
 end
